7ae6400753b3007f8f30123b9cd235d1c2a4c4b1
Myckel <m.uribe@portal53.cl>
2017-06-19 18:03:37 -0400

    create a insert controller and view

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,15 @@
 class PagesController < ApplicationController
   def x
+    @user = show_all_user
+  end
+  def save_user
+    if params[:name].present? && params[:last_name].present? && params[:email].present? && params[:age].present?
+        User.create(name: params[:name],last_name: params[:last_name],email: params[:email],age: params[:age]) 
+        redirect_to :back 
+    end
+    
+  end
+  def show_all_user
+    User.all
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
   root 'pages#x'
-  get 'pages/x'
+
+  post 'pages/x'
+
+  post 'pages/save_user'
+  
+  post 'pages/show_all_user'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
