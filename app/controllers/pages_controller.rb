01dcb291fb019af25432f4e6c30eae265cc89816
Myckel <m.uribe@portal53.cl>
2017-06-19 20:08:07 -0400

    add alert notice

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -7,10 +7,13 @@ class PagesController < ApplicationController
   end
   def save_user
     if params[:name].present? && params[:last_name].present? && params[:email].present? && params[:age].present?
-        User.create(name: params[:name],last_name: params[:last_name],email: params[:email],age: params[:age]) 
-        redirect_to :back 
+        @user=User.new(name: params[:name],last_name: params[:last_name],email: params[:email],age: params[:age]) 
+        @user.save
+        redirect_to :back, notice:"Usuario creado correctamente" 
+    else    
+        redirect_to :back, alert: "Complete todos los campos"
     end
-    
+
   end
   def show_all_user
     @user=User.all
